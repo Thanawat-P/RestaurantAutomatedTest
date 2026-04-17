26acaa6e10a0b26de63bcb85e8f6c271822b7e91
Thanawat-P <able14062002@gmail.com>
2026-04-17 15:16:47 +0700

    wait for example cases

---
+++ pages/staff.page.js
@@ -0,0 +1,17 @@
+class StaffAuthority{
+     construct(page) {
+        this.page= page;
+    } 
+    async gotoTable() {
+        await this.page.goto('https://example.com/table');
+    }
+
+    async updateStatus(table,status){
+         const row = page.locator('tr').filter({
+            hasText: `${table}`
+        });
+        const dropdown = row.locator('select');
+        await dropdown.selectOption({ label: status });
+    }
+}
+module.exports = {StaffAuthority}
